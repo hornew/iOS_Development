477d4a037ce75530ac1222d3c9823f5e9cbef010
hornew <hornew@goldmail.etsu.edu>
2013-11-26 19:31:15 -0500

    Initial Commit

---
+++ EmailDemo/EDViewController.h
@@ -0,0 +1,13 @@
+//
+//  EDViewController.h
+//  EmailDemo
+//
+//  Created by Horne, William Austin on 11/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDViewController : UIViewController
+
+@end
---
+++ EmailDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmailDemo
+//
+//  Created by Horne, William Austin on 11/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EDAppDelegate class]));
+    }
+}
